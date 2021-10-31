f404d5095fbab939924a88956490b3e05d2f03cb
michaelm3 <michael.muniz94@gmail.com>
2021-10-31 19:40:38 -0400

    added intro file

---
+++ intro.py
@@ -0,0 +1,19 @@
+# input function will ask you for input and 
+# the code will wait for a response
+username = input("What is your username? ")
+
+# print function will output to the terminal
+# whatever values you put between the parenthesis
+print(f"Welcome {username} it's a pleasure!")
+
+# brackets represent a list, List is MULTIPLE pieces of data together
+colors = ["red", "blue", "green", "purple", "black", "white", "grey"]
+
+someString = "Hello, World!" # STRING
+num = 5 # INTEGER (WHOLE NUMBER)
+floatNum = 3.5 # FLOAT (DECIMAL/FLOATING POINT)
+
+# curly braces represent a dictionary which is a key/value pair
+# all keys are strings, all values can be any datatype including 
+# another dictionary
+person = {"name": "Nick", "hair_color": ["black", "white"], "age": 39, "height": "5'11"}
